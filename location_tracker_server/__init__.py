076af3dccc477955f1cdf6b2ca4da3fb6aeee10c
Alan <alan.wernik@gmail.com>
2016-10-28 19:44:15 -0500

    Use flask-migrate to handle database

--- location_tracker_server/__init__.py
+++ location_tracker_server/__init__.py
@@ -1,9 +1,13 @@
+from os import path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/location_tracker'
+database_path = path.join(app.root_path, 'tmp', 'development.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(database_path)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import views
 
