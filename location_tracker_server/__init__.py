adfafa76d7a36713f704ead1633d0b7b9c9b2088
Alan <alan.wernik@gmail.com>
2016-10-28 20:28:43 -0500

    Use bcrypt to generate encrypted password

--- location_tracker_server/__init__.py
+++ location_tracker_server/__init__.py
@@ -2,12 +2,14 @@ from os import path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 database_path = path.join(app.root_path, 'tmp', 'development.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(database_path)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
 
 import views
 
--- location_tracker_server/models.py
+++ location_tracker_server/models.py
@@ -1,4 +1,4 @@
-from . import db
+from . import db, bcrypt
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -9,5 +9,16 @@ class User(db.Model):
     auth_token = db.Column(db.Text)
     auth_expires_at = db.Column(db.Date)
 
-    def __init__(self, name, email, encrypted_password):
-        pass
+    def __init__(self, name, email, password, password_confirmation):
+        self.name = name
+        self.email = email
+        self.__generate_encrypted_password(password, password_confirmation)
+
+    def __generate_encrypted_password(self, password, password_confirmation):
+        if password == password_confirmation:
+            if len(password) > 8:
+                self.encrypted_password = bcrypt.generate_password_hash(password)
+            else:
+                raise Exception('Password should be greater than or equal to 8 characters')
+        else:
+            raise Exception('Password and confirmation do not match')
